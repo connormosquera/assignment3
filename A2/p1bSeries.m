fa0abac9342eadeafef8da49b7e40060fcdc88c3
Connor Mosquera <mosquera.connor@gmail.com>
2018-02-21 08:34:40 -0500

    A2 p2 now working

--- A2/p1bSeries.m
+++ A2/p1bSeries.m
@@ -5,17 +5,16 @@ close all
 W = 100;
 L = 3/2*W;
 Vo = 1;
-Terms = 30;
+Terms = 60;
 
 V = zeros(L,W);
 
-for i=1:L
+for k=1:Terms
     for j=1:W
-        for k=1:Terms
-            V(i,j) = 4*V0/pi*1/k...;
+        for i=1:L
+            V(i,j) = V(i,j) + 4*Vo/pi*1/(2*k-1)*cosh((2*k-1)*pi*(i-1)/W)/cosh((2*k-1)*pi*L/W)*sin((2*k-1)*pi*(j-1)/W);
         end
     end
+    surf(V);
+    pause(0.05);
 end
-
-surf(Vmap);
-pause(0.001);
--- A2/p2a.m
+++ A2/p2a.m
@@ -12,33 +12,141 @@ delta = 1;
 
 V = zeros(L,W);
 V(L/2,W)=1;
-
 Sigma = ones(L,W);
+
 for i=1:Wb
-    for j=round(L/2-Lb):round(L/2+Lb)
+    for j=round(L/2-Lb/2):round(L/2+Lb/2)
         Sigma(i,j) = 0.01;
     end
 end
 for i=round(W-Wb):W
-    for j=round(L/2-Lb):round(L/2+Lb)
+    for j=round(L/2-Lb/2):round(L/2+Lb/2)
         Sigma(i,j) = 0.01;
     end
 end
 
-Vtemp=V;
+surf(Sigma);
 
-for h=1:maxI
-    for i=2:L-1
-        for j=2:W-1
-            Vtemp(i,j) = 1/(4*(Sigma(i,j)*delta^2))*(Sigma(i,j)*(V(i-1,j) ...
-            + V(i+1,j) + V(i,j-1) + V(i,j+1))+ ...
-            (Sigma(i+1,j)-Sigma(i-1,j))*(V(i+1,j)-V(i-1,j))+ ...
-            (Sigma(i,j+1)-Sigma(i,j-1))*(V(i,j+1)-V(i,j-1)));
+G = sparse(L*W,L*W);
+B = zeros(1,L*W);
+
+for i=1:L
+    for j=1:W
+        n = j + (i-1)*W;
+        
+        if i==1 && j==W/2
+            G(n,:) = 0;
+            G(n,n) = 1;
+            B(n) = Vo;
+        elseif i==L && j==W/2
+            G(n,:) = 0;
+            G(n,n) = 1;
+        elseif j==1 && i==1
+            G(n,:) = 0;
+            G(n,n) = -(1/Sigma(i+1,j) + 1/Sigma(i,j+1));
+            G(n,n+1) = 1/Sigma(i,j+1);
+            G(n,n+W) = 1/Sigma(i+1,j); 
+        elseif j==W && i==1
+            G(n,:) = 0;
+            G(n,n) = -(1/Sigma(i+1,j) + 1/Sigma(i,j-1));
+            G(n,n-1) = 1/Sigma(i,j-1);
+            G(n,n+W) = 1/Sigma(i+1,j);
+        elseif j==1 && i==L
+            G(n,:) = 0;
+            G(n,n) = -(1/Sigma(i-1,j) + 1/Sigma(i,j+1));
+            G(n,n+1) = 1/Sigma(i,j+1);
+            G(n,n-W) = 1/Sigma(i-1,j); 
+        elseif j==W && i==L
+            G(n,:) = 0;
+            G(n,n) = -(1/Sigma(i-1,j) + 1/Sigma(i,j-1));
+            G(n,n-1) = 1/Sigma(i,j-1);
+            G(n,n-W) = 1/Sigma(i-1,j);    
+        elseif j==1
+            G(n,:) = 0;
+            G(n,n) = -(1/Sigma(i+1,j) + 1/Sigma(i,j+1) + ...
+                1/Sigma(i-1,j));
+            G(n,n+1) = 1/Sigma(i,j+1);
+            G(n,n+W) = 1/Sigma(i+1,j); 
+            G(n,n-W) = 1/Sigma(i-1,j);
+        elseif j==W
+            G(n,:) = 0;
+            G(n,n) = -(1/Sigma(i+1,j) + 1/Sigma(i,j-1) + ...
+                1/Sigma(i-1,j));
+            G(n,n-1) = 1/Sigma(i,j-1);
+            G(n,n+W) = 1/Sigma(i+1,j); 
+            G(n,n-W) = 1/Sigma(i-1,j);
+        elseif i==1
+            G(n,:) = 0;
+            G(n,n) = -(1/Sigma(i+1,j) + 1/Sigma(i,j+1) + ...
+                1/Sigma(i,j-1));
+            G(n,n+1) = 1/Sigma(i,j+1);
+            G(n,n+W) = 1/Sigma(i+1,j); 
+            G(n,n-1) = 1/Sigma(i,j-1);
+        elseif i==L
+            G(n,:) = 0;
+            G(n,n) = -(1/Sigma(i,j+1) + 1/Sigma(i,j-1) + ...
+                1/Sigma(i-1,j));
+            G(n,n-1) = 1/Sigma(i,j-1);
+            G(n,n+1) = 1/Sigma(i,j+1); 
+            G(n,n-W) = 1/Sigma(i-1,j);  
+        else
+            G(n,:) = 0;
+            G(n,n) = -(1/Sigma(i,j+1) + 1/Sigma(i,j-1) + ...
+                1/Sigma(i-1,j) + 1/Sigma(i+1,j));
+            G(n,n-1) = 1/Sigma(i,j-1);
+            G(n,n+1) = 1/Sigma(i,j+1); 
+            G(n,n-W) = 1/Sigma(i-1,j); 
+            G(n,n+W) = 1/Sigma(i+1,j); 
         end
+        
     end
-    V=Vtemp;
-    surf(V);
-    pause(0.001);
 end
 
+spy(G);
 
+F=G\B';
+
+Vmap = zeros(L,W);
+
+for i=1:L
+    for j=1:W
+        n = j + (i-1)*W;
+        Vmap(i,j) = F(n);
+    end
+end
+
+figure(2)
+hold on
+surf(Vmap);
+hold off
+pause(0.001);
+
+[Ex,Ey]=gradient(Vmap);
+
+% Ex=zeros(L,W);
+% Ey=zeros(L,W);
+% 
+% for i=1:L
+%     for j=1:W
+%         if i==1
+%             Ex(i,j) = Vmap(i+1,j) - Vmap(i,j);
+%         elseif i==L
+%             Ex(i,j) = Vmap(i,j) - Vmap(i-1,j);
+%         else
+%             Ex(i,j) = Vmap(i+1,j) - Vmap(i-1,j);
+%         end
+%         if j==1
+%             Ey(i,j) = Vmap(i,j+1) - Vmap(i,j);
+%         elseif j==W
+%             Ey(i,j) = Vmap(i,j) - Vmap(i,j-1);
+%         else
+%             Ey(i,j) = Vmap(i,j+1) - Vmap(i,j-1);
+%         end
+%     end
+% end
+
+figure(3)
+hold on
+quiver(Ex,Ey);
+hold off
+pause(0.001);
